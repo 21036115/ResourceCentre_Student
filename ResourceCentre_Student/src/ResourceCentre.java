1438a5e5801ca3071e7a13942120ba9c07805457
User <User@LAPTOP-EKJTMOAU>
2022-07-14 13:39:34 +0800

    Test 5

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -266,6 +266,7 @@ public class ResourceCentre {
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here
+		isReturned = false;
 		return isReturned;
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
